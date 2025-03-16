fecbe5e5be5e2a3d75c46dc577dc23fbf76bf457
BugKiller099 <myashifjamal@gmail.com>
2025-03-16 19:58:21 +0530

    added logout api

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -2,6 +2,7 @@ const express = require('express');
 const {validateSignUpData } = require("../utils/validation");
 const User = require("../models/user");
 const bcrypt = require("bcrypt");
+const req = require('express/lib/request');
 const authRouter = express.Router();
 
 module.exports = authRouter;
@@ -57,5 +58,13 @@ authRouter.post("/login" , async (req, res)=>{
     
 });
 
+authRouter.post("/logout" ,async (req, res) =>{
+   res.cookie("token", null, {
+    expires: new Date(Date.now()),
+
+   });
+
+   res.send("Logout Susseccful");
+});
 
 module.exports = authRouter;
