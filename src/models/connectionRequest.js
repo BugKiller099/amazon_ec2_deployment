1bdd39630e5897c2f1712c62188f55582e961ec6
BugKiller099 <myashifjamal@gmail.com>
2025-03-20 07:15:05 +0530

    used compound indexes in connectionRequest.js

--- src/models/connectionRequest.js
+++ src/models/connectionRequest.js
@@ -22,6 +22,17 @@ const connectionRequestSchema = new mongoose.Schema({
 },
 { timestamps: true},
 );
+
+connectionRequestSchema.pre("save" , function(next){
+    const connectionRequest = this;
+    //check if the fromUserId is same as toUserId
+
+    if(connectionRequest.fromUserId.equals(connectionRequest.toUserId)){
+        throw new Error("Cannot send connection request to yourself!");
+    }
+    next();
+
+});
 connectionRequestSchema.index({fromUserId: 1, toUserId: 1},{unique: true});
 const connectionRequestModel = new mongoose.model("ConnectionRequest", connectionRequestSchema );
 
